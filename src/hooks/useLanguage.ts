5aa41d49778e561f5e0070604f86f04aa74b999f
Mohamed Amine Macherki <mohamedamine.macherki@ieee.org>
2025-07-21 02:21:03 +0100

    fix issue

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { translations } from '../../../../project-bolt-sb1-bbnc7h3v/project/src/data/translations';
 
 export type LanguageCode = 'fr' | 'en' | 'it' | 'es';
 
@@ -8,9 +7,6 @@ export const useLanguage = () => {
 
   useEffect(() => {
     const savedLanguage = localStorage.getItem('portfolio-language') as LanguageCode;
-    if (savedLanguage && translations[savedLanguage]) {
-      setCurrentLanguage(savedLanguage);
-    }
   }, []);
 
   const changeLanguage = (language: LanguageCode) => {
@@ -18,7 +14,4 @@ export const useLanguage = () => {
     localStorage.setItem('portfolio-language', language);
   };
 
-  const t = translations[currentLanguage];
-
-  return { currentLanguage, changeLanguage, t };
 };
